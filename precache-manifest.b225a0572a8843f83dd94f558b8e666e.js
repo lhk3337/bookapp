9c7bb2ec8155b28d14f8cbacac17da59e51b0c6b
HK <user@HK.local>
2020-02-05 14:13:06 +0900

    Updates

---
+++ precache-manifest.b225a0572a8843f83dd94f558b8e666e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae00503ac247b6499578c428a2c52ab8",
+    "url": "/bookapp/index.html"
+  },
+  {
+    "revision": "a7f55ebf818ade9c2aac",
+    "url": "/bookapp/static/css/main.d1ce502d.chunk.css"
+  },
+  {
+    "revision": "1df2947fc03da6afca51",
+    "url": "/bookapp/static/js/2.8478c029.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bookapp/static/js/2.8478c029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7f55ebf818ade9c2aac",
+    "url": "/bookapp/static/js/main.986007f4.chunk.js"
+  },
+  {
+    "revision": "d718551bfce443f79987",
+    "url": "/bookapp/static/js/runtime-main.c967bd4e.js"
+  }
+]);
